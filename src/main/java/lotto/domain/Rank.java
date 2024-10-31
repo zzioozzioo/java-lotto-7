84dd9d49b28539c708526233699ef8dc28ea3978
zzioozzioo <zzioo1006@naver.com>
2024-10-31 22:26:15 +0900

    feat: 출력 기능 구현
    - 발행한 로또 수량, 로또 번호, 당첨 내역, 수익률 출력 기능 구현
    - 당첨 내역 출력을 위해 Rank에 stringPrize 필드 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,27 +1,22 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000L, "2,000,000,000"),
+    SECOND(5, 30_000_000L, "30,000,000"),
+    THIRD(5, 1_500_000L, "1,500,000"),
+    FOURTH(4, 50_000L, "50,000"),
+    FIFTH(3, 5_000L, "5,000"),
+    NONE(0, 0L, "0");
 
     private final int score;
     private final long prize;
 
-    Rank(int score, long prize) {
+    private final String stringPrize;
+
+    Rank(int score, long prize, String stringPrize) {
         this.score = score;
         this.prize = prize;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public long getPrize() {
-        return prize;
+        this.stringPrize = stringPrize;
     }
 
     public static Rank findRank(int score, boolean isBonusMatches) {
@@ -41,5 +36,17 @@ public enum Rank {
             return FIFTH;
         }
         return NONE;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+
+    public String getStringPrize() {
+        return stringPrize;
     }
 }
