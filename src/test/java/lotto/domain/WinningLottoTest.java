a3827e27f6483dde7301649f7c9658e6326f4ded
zzioozzioo <zzioo1006@naver.com>
2024-11-03 20:02:24 +0900

    test: WinningLotto에 대한 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -6,19 +6,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class WinningLottoTest {
+class WinningLottoTest {
 
     @Test
     void 로또_번호와_당첨_번호_비교_테스트() {
         //given
-        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
 
         Lotto winningLottoNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         WinningLotto winningLotto = new WinningLotto(winningLottoNumbers);
 
         //when
-        int matchCount = winningLotto.countMatchingNumbers(userLotto);
+        int matchCount = winningLotto.countMatchingNumbers(lotto);
 
         //then
         assertThat(matchCount).isEqualTo(5);
