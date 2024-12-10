dc18958e69e4152beb8ae5f4e432f75bc048424e
zzioozzioo <zzioo1006@naver.com>
2024-12-10 22:32:22 +0900

    refactor: Lotto 클래스를 domain 패키지로 이동함으로써 테스트 코드의 패키지 이동

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,10 @@
-package lotto;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
